c5d70b54c9424dcb8c517acee3ea35672fcabd83
crux-hironori <hironori0811@gmail.com>
2020-06-06 09:58:31 +0900

    added retrun and end buttons

--- sample-project/src/components/AnswerCheck.js
+++ sample-project/src/components/AnswerCheck.js
@@ -1,4 +1,8 @@
 import React from "react";
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import { BrowserRouter} from 'react-router-dom';
 
 export default class AnswerCheck extends React.Component {
     constructor(props) {
@@ -6,17 +10,24 @@ export default class AnswerCheck extends React.Component {
         this.state = {
             answer: "",
             result: "",
+            isAnswered: false,
         };
         this.handleChange = this.handleChange.bind(this);
+        this.backTOMain = this.backTOMain.bind(this)
     }
 
     answerCheck(answer, sum) {
         const isCorrect = answer == sum ? "正解" : "不正解";
-        this.setState({ result: isCorrect });
+        this.setState({ result: isCorrect,
+                        isAnswered: true });
     }
 
     handleChange(e) {
         this.setState({ answer: e.target.value });
+    }
+
+    backTOMain(){
+        this.props.history.push('/');
     }
 
     render() {
@@ -25,14 +36,31 @@ export default class AnswerCheck extends React.Component {
         return (
             <div>
                 <input type="number"
+                    disabled={this.state.isAnswered}
                     value={this.state.answer}
                     onChange={(e) => this.handleChange(e)}>
 
                 </input>
-                <button onClick={() => this.answerCheck(this.state.answer, numbers.reduce(sum))}>
+                <Button
+                    disabled={this.state.isAnswered} 
+                    onClick={() => this.answerCheck(this.state.answer, numbers.reduce(sum))}
+                    >
                     回答
-                    </button>
+                </Button>
                 <div>{this.state.result}</div>
+                {this.state.isAnswered?
+                    <div><Button 
+                            size="sm"
+                            variant="outline-primary">
+                                もう一回
+                                </Button>
+                                {' '}
+                        <Button 
+                            size="sm"
+                            variant="outline-primary"
+                            onClick={this.backTOMain}>
+                            終わる</Button>
+                    </div>:null}
             </div>
         );
     }
