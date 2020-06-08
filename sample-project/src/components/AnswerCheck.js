0ae91383c490ac5a7a3e9d958165d053cd05abbd
crux-hironori <hironori0811@gmail.com>
2020-06-08 13:18:49 +0900

    edited input for answer #6

--- sample-project/src/components/AnswerCheck.js
+++ sample-project/src/components/AnswerCheck.js
@@ -1,8 +1,11 @@
 import React from "react";
+import ReactDOM from 'react-dom'
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import { BrowserRouter} from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export default class AnswerCheck extends React.Component {
     constructor(props) {
@@ -25,22 +28,34 @@ export default class AnswerCheck extends React.Component {
         this.setState({ answer: e.target.value });
     }
 
+    componentDidMount() {
+        let input = ReactDOM.findDOMNode(this.refs.answer);
+        input && input.focus();
+    }
+
     render() {
         const numbers = this.props.numbers;
         const sum = (accumulator, currentValue) => accumulator + currentValue;
         return (
             <BrowserRouter> 
             <div>
+            <Row className="justify-content-md-center">
+            <Col md="6">
                 <input type="number"
                     disabled={this.state.isAnswered}
                     value={this.state.answer}
-                    onChange={(e) => this.handleChange(e)}>
+                    onChange={(e) => this.handleChange(e)}
+                    width="auto"
+                    ref="answer">
                 </input>
+                {' '}
                 <Button
                     disabled={this.state.isAnswered} 
                     onClick={() => this.answerCheck(this.state.answer, numbers.reduce(sum))}>
                     回答
                 </Button>
+                </Col>
+                </Row>
                 <div>{this.state.result}</div>
                 {this.state.isAnswered?
                     <div><Button 
